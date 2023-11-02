8d915f7160d774f4291dfba2d2fbb737b5c39e2a
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-11-03 00:37:10 +0300

    create profile request

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\AddressType;
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\Country;
+use App\Models\CustomerAddress;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,6 +19,14 @@ class ProfileController extends Controller
      */
     public function view(Request $request)
     {
-        return view('profile.view');
+        /** @var \App\Models\User $user */
+        $user = $request->user();
+        /** @var \App\Models\Customer $customer */
+        $customer = $user->customer;
+        $shippingAddress = $customer ? $customer->shippingAddress : new CustomerAddress(['type' => AddressType::Shipping]);
+        $billingAddress = $customer ? $customer->billingAddress : new CustomerAddress(['type' => AddressType::Billing]);
+    //    dd($customer, $shippingAddress->attributesToArray(), $billingAddress, $billingAddress->customer);
+        $countries = Country::query()->orderBy('name')->get();
+        return view('profile.view', compact('customer', 'user', 'shippingAddress', 'billingAddress', 'countries'));
     }
 }
